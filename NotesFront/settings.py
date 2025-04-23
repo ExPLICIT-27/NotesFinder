6374249077332654036569284e68fa6020288f2c
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-04-23 18:15:19 +0530

    Some more fixes

--- NotesFront/settings.py
+++ NotesFront/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # if you have a /static dir in your project
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # if you have a /static dir in your project
 
 
 # Media files (User uploads)
